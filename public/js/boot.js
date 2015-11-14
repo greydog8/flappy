fcc2ded70b0be975d0b061bd40eb755a7a8518cb
Greyson <greydog8@skilstak.sh>
2015-11-14 11:12:16 -0500

    save

--- public/js/boot.js
+++ public/js/boot.js
@@ -4,10 +4,19 @@
 'use strict';
 
 var Flappy = { };
-
+Flappy.Boot = function () {};
 Flappy.Boot.prototype = {
 
   init: function () {
+  },
+
+  preload: function ()  {
+  },
+
+  create: function () {
+    this.game.stage.backgroundColor = "#FF0000";
+  }
+};
   
     
   
